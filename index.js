d0b6e59be536fb7176cd16519d7be2f51a3ff4a4
Abdullah <abdullaharif059@gmail.com>
2022-09-05 14:04:51 +0530

    js changes

--- index.js
+++ index.js
@@ -1,105 +1,54 @@
 let dataSec = [];
 let objData = [];
 let dataArr = [];
-let card = document.getElementsByClassName('card');
 let A1 = document.getElementById('cardId');
-document.getElementById('searchBar').addEventListener("input", searchFunc)
-function searchFunc(e) {
-    let value = e.target.value;
-    let result = Object.values(dataArr);
-    // console.log(result);
-    // console.log(dataArr);
-    console.log(result.filter(values => values.name));
-}
+
 fetch("https://api.spacexdata.com/v4/launches").then((data) => data.json())
     .then((objData) => {
         dataArr = objData.map((values) => {
             dataSec += `
-        <div class="card">
-        <div class="img">
-            <img src="${values.links.patch.small}" id="img" alt="">
-        </div>
-        <div class="des-cont">
-            <h3 id="name">${values.flight_number + ". " + values.name}</h3>
-            <div class="">
-                <p id="details">${values.details + "."}
+            <div class="card">
+            <div class="img">
+                <img src="${values.links.patch.small}" id="img" alt="">
+            </div>
+            <div class="text-flex">
+                <h3 class="name">${values.flight_number + ". " + values.name}</h3>
+                <p class="details">${values.details + "."}
                 </p><br>
                 <a href="${values.links.wikipedia}" class="btn" target="_blank">
-                Read More</a>
-            </div>  
+                    Read More</a>
+            </div>
         </div>
-    </div>
         `;
             A1.innerHTML = dataSec;
-            return {
-                name: values.name, img:
-                    values.links.patch.small, flightNumber: values.flight_number,
-                details: values.details,
-                links: values.links.wikipedia,
-                Element: card
-            }
         })
     })
 
+function inputFunc() {
+    // let value = document.getElementById('searchBar').value;
+    let card = document.getElementsByClassName("card")
+    var searchBar = document.getElementById('searchBar').value.toLowerCase();
+    var eventCatch = event.target.value;
+    console.log(eventCatch);
+    console.log(card);
+    var name = document.getElementsByClassName('name');
+    var details = document.getElementsByClassName('details')
+    console.log(details)
+    var textFlex = document.getElementsByClassName('text-flex');
+    console.log(textFlex)
+    console.log(name);
+    for (let i = 0; i < name.length; i++) {
+        let card = textFlex[i].parentNode
 
-// function funcbtn() {
-//     console.log('btn is clicked');
-//     let btn = document.getElementById('btn')
-//     let link = document.getElementById('link')
-//     if (link.style.display === 'none') {
-//         link.style.display = 'Block'
-//         btn.innerHTML = 'collapse'
-//     }
-//     else {
-//         link.style.display = 'none'
-//         btn.innerHTML = 'expand'
-//     }
-// }
-// function funcbtn() {
-//     console.log('btn is clicked');
-//     let btn = document.getElementsByClassName('btn');
-//     let link = document.getElementsByClassName('link');
-//     console.log('it works', link)
-//     if (link.style.display === 'none') {
-//         link.style.display = 'Block'
-//         btn.innerHTML = 'collapse'
-//     }
-//     else {
-//         link.style.display = 'none'
-//         btn.innerHTML = 'expand'
-//     }
-// }
+        if (name[i].innerHTML.toLowerCase().includes(eventCatch) ||
+            details[i].innerHTML.toLowerCase().includes(eventCatch)
+        ) {
+            card.style.display = 'flex'
+            console.log(name[i]);
+        }
+        else {
+            card.style.display = 'none';
+        }
+    }
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// fetch("https://api.spacexdata.com/v4/launches").then((data) => {
-//     return data.json();
-// }).then((objData) => {
-//     console.log(objData);
-//     let dataSec = "";
-//     objData.map((values) => {
-//         dataSec += `
-//         <div id="abcd">
-//         <img src="${values.links.patch.small}" >
-//         <h1>${values.name}</h1>
-//         </div>
-//         `;
-//         console.log('hello World')
-//         console.log(values)
-//         let A1 = document.getElementById('cardId');
-//         A1.innerHTML = dataSec;
-//     })
-// })
